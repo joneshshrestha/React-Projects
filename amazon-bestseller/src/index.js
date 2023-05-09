192256ca777c490f9d2c60ce1ec5719ed715833a
joneshshrestha <contact@joneshshrestha.com>
2023-05-09 21:18:45 +0545

    update

--- amazon-bestseller/src/index.js
+++ amazon-bestseller/src/index.js
@@ -2,38 +2,50 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const Booklist = () => {
+const firstBook = {
+  author: 'James Clear',
+  title: 'Atomic Habits: An Easy & Proven Way to Build',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
+}
+
+const secondBook = {
+  author: 'Bonnie Garmus',
+  title: 'Lessons in Chemistry: A Novel',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL600_SR600,400_.jpg',
+}
+
+const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      >
+        <p>hello world this is a children prop which is passed</p>
+      </Book>
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   )
 }
 
-const Book = () => {
+const Book = ({ author, title, img, children }) => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt={title} />
+      <h3>{title}</h3>
+      {children}
+      <h4>{author}</h4>
     </article>
   )
 }
 
-const Image = () => (
-  <img
-    src="./images/book1.jpg"
-    alt="Atomic Habits: An Easy & Proven Way to Build"
-  />
-)
-
-const Title = () => <h3>Atomic Habits: An Easy & Proven Way to Build</h3>
-const Author = () => <h4>James Clear</h4>
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Booklist />)
+root.render(<BookList />)
 
-export default Booklist
+export default BookList
