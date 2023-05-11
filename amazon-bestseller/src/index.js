5643229990a5180fc99ea0524b7636d9e4158b1b
joneshshrestha <contact@joneshshrestha.com>
2023-05-11 21:28:37 +0545

    update

--- amazon-bestseller/src/index.js
+++ amazon-bestseller/src/index.js
@@ -2,45 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const firstBook = {
-  author: 'James Clear',
-  title: 'Atomic Habits: An Easy & Proven Way to Build',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
-}
-
-const secondBook = {
-  author: 'Bonnie Garmus',
-  title: 'Lessons in Chemistry: A Novel',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL600_SR600,400_.jpg',
-}
-
-const names = ['Jonesh', 'Sakshi', 'Jena']
-
-const newNames = names.map((name) => {
-  return <h1>{name}</h1>
-})
+const books = [
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits: An Easy & Proven Way to Build',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
+  },
+  {
+    author: 'Bonnie Garmus',
+    title: 'Lessons in Chemistry: A Novel',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL600_SR600,400_.jpg',
+  },
+]
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author={newNames} title={firstBook.title} img={firstBook.img}>
-        <p>hello world this is a children prop which is passed</p>
-      </Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        const { author, title, img } = book
+        return <Book author={author} title={title} img={img} />
+      })}
     </section>
   )
 }
 
-const Book = ({ author, title, img, children }) => {
+const Book = ({ author, title, img }) => {
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h3>{title}</h3>
-      {children}
       <h4>{author}</h4>
     </article>
   )
