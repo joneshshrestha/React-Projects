3e67b2ab74f0370006f0e200b396aa6f48f3849d
joneshshrestha <contact@joneshshrestha.com>
2023-05-12 20:44:40 +0545

    update

--- amazon-bestseller/src/index.js
+++ amazon-bestseller/src/index.js
@@ -7,11 +7,13 @@ const books = [
     author: 'James Clear',
     title: 'Atomic Habits: An Easy & Proven Way to Build',
     img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
+    id: 1,
   },
   {
     author: 'Bonnie Garmus',
     title: 'Lessons in Chemistry: A Novel',
     img: 'https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL600_SR600,400_.jpg',
+    id: 2,
   },
 ]
 
@@ -19,14 +21,17 @@ const BookList = () => {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { author, title, img } = book
-        return <Book author={author} title={title} img={img} />
+        console.log(book)
+        return <Book book={book} />
       })}
     </section>
   )
 }
 
-const Book = ({ author, title, img }) => {
+const Book = (props) => {
+  console.log(props)
+  const { img, author, title } = props.book
+  console.log(props)
   return (
     <article className="book">
       <img src={img} alt={title} />
