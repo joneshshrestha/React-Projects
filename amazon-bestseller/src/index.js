7a736b5b615cabfc7a8a5f65b984404d1710d74f
joneshshrestha <contact@joneshshrestha.com>
2023-05-17 08:39:53 +0545

    update handle

--- amazon-bestseller/src/index.js
+++ amazon-bestseller/src/index.js
@@ -22,16 +22,41 @@ const books = [
 const BookList = () => {
   return (
     <section className="booklist">
+      <EventExamples />
       {books.map((book) => {
-        return <Book {...book} />
+        return <Book {...book} key={book.id} />
       })}
+    </section>
+  )
+}
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log('handle form input')
+  }
+
+  const handleButtonClick = () => {
+    alert('handle button click')
+  }
+
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>Click Me!</button>
     </section>
   )
 }
 
 const Book = (props) => {
   const { img, author, title } = props
-  console.log(props)
   return (
     <article className="book">
       <img src={img} alt={title} />
