71979c939eda28c1d0dd6daf9d0858a7165a3b72
joneshshrestha <contact@joneshshrestha.com>
2023-05-24 19:47:40 +0545

    update readme and js files

---
+++ backroads-app/src/components/Navbar.js
@@ -0,0 +1,77 @@
+import logo from '../images/logo.svg'
+
+function navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        {/* <!-- left this comment on purpose --> */}
+        <ul className="nav-links" id="nav-links">
+          <li>
+            <a href="#home" className="nav-link">
+              {' '}
+              home{' '}
+            </a>
+          </li>
+
+          <li>
+            <a href="#about" className="nav-link">
+              {' '}
+              about{' '}
+            </a>
+          </li>
+
+          <li>
+            <a href="#services" className="nav-link">
+              {' '}
+              services{' '}
+            </a>
+          </li>
+
+          <li>
+            <a href="#tours" className="nav-link">
+              {' '}
+              tours
+            </a>
+          </li>
+        </ul>
+
+        <ul className="nav-icons">
+          <li>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              className="nav-icon"
+            >
+              <i className="fab fa-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              className="nav-icon"
+            >
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              className="nav-icon"
+            >
+              <i className="fab fa-squarespace"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+export default navbar
